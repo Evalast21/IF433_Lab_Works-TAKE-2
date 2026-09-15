35922e5959177dc5448db91e7c11f5c235ff7c5c
Evalast21 <spectraleox57@gmail.com>
2026-09-15 23:58:23 +0700

    week02: implement selection logic for constructors

--- src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Main.kt
+++ src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Main.kt
@@ -1,5 +1,3 @@
-package oop_001_ataraxiaaria.week01.oop_00000148140_AtaraxiaAriaWicaksono_week02
-
 import java.util.Scanner
 
 fun main() {
@@ -13,16 +11,33 @@ fun main() {
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
 
-    scanner.nextLine()
+    scanner.nextLine() // Consume newline
 
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
 
-        val s1 = Student(name, nim, major)
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
 
-        println("Status: Pendaftaran Selesai.")
+        scanner.nextLine() // Consume newline
+
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+
+            val s1 = Student(name, nim, major)
+
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+
+        } else if (type == 2) {
+
+            val s2 = Student(name, nim)
+
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
