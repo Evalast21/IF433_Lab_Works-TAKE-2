21e78438e2fd7804051f4f5638c0b32e18da2f98
Evalast21 <spectraleox57@gmail.com>
2026-09-16 00:12:59 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Hero.kt.kt
@@ -0,0 +1,24 @@
+package oop_001_ataraxiaaria.week01.oop_00000148140_AtaraxiaAriaWicaksono_week02
+
+class Hero(
+    val name: String,
+    var hp: Int = 100,
+    val baseDamage: Int
+) {
+
+    fun attack(targetName: String) {
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
--- src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Student.kt.kt
+++ src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Student.kt.kt
@@ -1,6 +1,6 @@
 package oop_001_ataraxiaaria.week01.oop_00000148140_AtaraxiaAriaWicaksono_week02
 
-class Student(
+class `Student.kt`(
     val name: String,
     val nim: String,
     var major: String,
