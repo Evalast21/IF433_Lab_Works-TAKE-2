312f40995cfe430b3d7d2d23d5019dbe437ccb7d
Evalast21 <spectraleox57@gmail.com>
2026-09-15 23:50:14 +0700

    week02: challenge refactor constructor using default args

--- src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Student.kt
+++ src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Student.kt
@@ -3,7 +3,8 @@ package oop_001_ataraxiaaria.week01.oop_00000148140_AtaraxiaAriaWicaksono_week02
 class Student(
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ) {
     init {
         if (nim.length != 5) {
