b0339ce23ca130c5a762ceff061be79049f15521
Evalast21 <spectraleox57@gmail.com>
2026-09-16 00:07:23 +0700

    week02: (task 1) implement input system

--- src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Main.kt
+++ src/oop_00000148140_AtaraxiaAriaWicaksono_week02/Main.kt
@@ -1,43 +1,35 @@
+package oop_001_ataraxiaaria.week01.oop_00000148140_AtaraxiaAriaWicaksono_week02
+
 import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    println("--- APLIKASI PMB UMN ---")
+    println("--- LIBRARY FINE SYSTEM ---")
 
-    print("Masukkan Nama: ")
-    val name = scanner.nextLine()
+    print("Masukkan Judul Buku: ")
+    val bookTitle = scanner.nextLine()
 
-    print("Masukkan NIM (Wajib 5 Karakter): ")
-    val nim = scanner.next()
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
 
-    scanner.nextLine() // Consume newline
+    print("Masukkan Lama Pinjam (hari): ")
+    var loanDuration = scanner.nextInt()
 
-    if (nim.length != 5) {
-        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-    } else {
-
-        print("Pilih Jalur (1. Reguler, 2. Umum): ")
-        val type = scanner.nextInt()
-
-        scanner.nextLine() // Consume newline
-
-        if (type == 1) {
-            print("Masukkan Jurusan: ")
-            val major = scanner.nextLine()
-
-            val s1 = Student(name, nim, major)
-
-            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
-
-        } else if (type == 2) {
-
-            val s2 = Student(name, nim)
-
-            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
-
-        } else {
-            println("Pilihan ngawur, pendaftaran batal!")
-        }
+    if (loanDuration < 0) {
+        loanDuration = 1
     }
+
+    val loan = Loan(
+        bookTitle,
+        borrower,
+        loanDuration
+    )
+
+    println()
+    println("--- DETAIL PEMINJAMAN ---")
+    println("Judul Buku: ${loan.bookTitle}")
+    println("Peminjam: ${loan.borrower}")
+    println("Lama Pinjam: ${loan.loanDuration} hari")
+    println("Total Denda: Rp ${loan.calculateFine()}")
 }
