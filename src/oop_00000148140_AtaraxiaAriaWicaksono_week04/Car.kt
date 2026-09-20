368b222164204f86719a541c0d17a0543aaac01f
Evalast21 <spectraleox57@gmail.com>
2026-09-20 13:31:03 +0700

    week04: test Vehicle and Car in main

--- src/oop_00000148140_AtaraxiaAriaWicaksono_week04/Car.kt
+++ src/oop_00000148140_AtaraxiaAriaWicaksono_week04/Car.kt
@@ -1,21 +1,19 @@
 package oop_001_ataraxiaaria.week01.oop_00000148140_AtaraxiaAriaWicaksono_week04
 
-class Car{
-    open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
 
         fun openTrunk(){
             println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka. ")
         }
     }
 
-    class ToyotaCar: Vehicle(brand = String()){
-        override fun honk() {
-            println("TIN TIN! Mobil $brand lewat!")
-        }
+class ToyotaCar(numberOfDoors: Int) : Car(brand = "Toyota", numberOfDoors = numberOfDoors) {
+    override fun honk() {
+        println("TIN TIN! Mobil $brand lewat!")
+    }
 
-        override fun accelerate() {
-            super.accelerate()
-            println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
-        }
+    override fun accelerate() {
+        super.accelerate()
+        println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
     }
 }
---
+++ src/oop_00000148140_AtaraxiaAriaWicaksono_week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_001_ataraxiaaria.week01.oop_00000148140_AtaraxiaAriaWicaksono_week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
